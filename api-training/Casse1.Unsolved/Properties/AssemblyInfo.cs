245dd8ddc450f918ae0bbdd3b27ae388dd1c120e
banks <alsb@itu.dk>
2018-10-03 10:55:11 +0200

    fixeroo

--- api-training/Casse1.Unsolved/Properties/AssemblyInfo.cs
+++ api-training/Casse1.Unsolved/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Case1")]
+[assembly: AssemblyTitle("Case1.Unsolved")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Case1")]
+[assembly: AssemblyProduct("Case1.Unsolved")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
