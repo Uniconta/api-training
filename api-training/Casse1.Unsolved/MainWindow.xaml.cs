ddd970244a3e44b2d833acb36a322efc3db8f155
banks <alsb@itu.dk>
2018-10-03 10:46:17 +0200

    Add Case1 unsolved

---
+++ api-training/Casse1.Unsolved/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows;
+using Uniconta.API.Service;
+using Uniconta.Common;
+using Uniconta.Common.User;
+
+namespace Case1.Unsolved
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // TODO: Insert API Key
+        private Guid APIKey { get; } = new Guid("00000000-0000-0000-0000-000000000000");
+
+        public MainWindow()
+        {
+            // TODO: Initialize Connection and Session
+            InitializeComponent();
+        }
+
+        private async void LoginBtn_Click(object sender, RoutedEventArgs e)
+        {
+            // Retrieve username and password from input fields
+            var userName = UsernameTextbox.Text;
+            var passWord = PasswordTextbox.Password;
+
+            // TODO: Login
+
+            // Succesfully logged in. Navigate to next window
+            new LoggedInWindow().Show();
+            this.Close();
+
+        }
+    }
+}
