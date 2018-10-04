69423ba7ff875db0bc6d2e6b73af4caac66a8f80
Philip Korsholm <pk@uniconta.com>
2018-10-04 10:53:17 +0200

    added projects

--- api-training/Case3/Properties/AssemblyInfo.cs
+++ api-training/Case3/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Case2")]
+[assembly: AssemblyTitle("Case3")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Case2")]
+[assembly: AssemblyProduct("Case3")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
