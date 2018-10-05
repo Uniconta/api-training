b52a0b4763b9341d502e747f408412637e366aae
Banks <alsb@itu.dk>
2018-10-05 17:23:23 +0200

    add nuget packages to all projects

---
+++ api-training/Case2/UnicontaManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Case2
+{
+    public static class UnicontaManager
+    {
+        public static int MyProperty { get; set; }
+
+    }
+}
