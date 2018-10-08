1ba1e8810d8b3abbbeeeddc57db1ba3e7fabc031
Philip Korsholm <pk@uniconta.com>
2018-10-08 18:10:24 +0200

    added case7 + unsolved

---
+++ api-training/Case7.Unsolved/PageEvents.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Uniconta.API.Plugin;
+using Uniconta.Common;
+
+namespace Case7.Unsolved
+{
+    public class PageEvents : PageEventsBase
+    {
+        public override ErrorCodes OnUpdate(UnicontaBaseEntity record, UnicontaBaseEntity original)
+        {
+            //TODO: save the original object in the database with a different item number, keeping them both.
+            return base.OnUpdate(record, original);
+        }
+    }
+}
---
+++ api-training/Case7/PEB.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Uniconta.API.Plugin;
+using Uniconta.ClientTools.DataModel;
+using Uniconta.Common;
+
+namespace Case7
+{
+    public class PEB : PageEventsBase
+    {
+        public override ErrorCodes OnUpdate(UnicontaBaseEntity record, UnicontaBaseEntity original)
+        {
+            var clone = StreamingManager.Clone(original);
+            var baserRes =  base.OnUpdate(record, original);
+            (clone as InvItemClient).Item = (clone as InvItemClient).Item + 1;
+            var res = api.Insert(clone).Result;
+            return baserRes;
+        }
+    }
+}
