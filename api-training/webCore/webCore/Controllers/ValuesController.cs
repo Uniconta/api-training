7c09502823e65cb2926d3e35274decf7c2fe1c13
Philip Korsholm <pk@uniconta.com>
2018-10-10 15:00:46 +0200

    Added simple web core API

---
+++ api-training/webCore/webCore/Controllers/ValuesController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Uniconta.Common;
+
+namespace webCore.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+        
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public IActionResult Post(LoginModel loginmodel)
+        {
+            //var loginmodel = new LoginModel();
+            if (UnicontaManager.Login(loginmodel.UserName, loginmodel.password).Result)
+            {
+                UnicontaManager.setCompany(11940);
+                return Ok();
+            }
+            return NotFound();
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
