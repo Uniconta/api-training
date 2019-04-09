921fe7cef123de97527a03aa1032f5909f4a7514
banks <alsb@itu.dk>
2019-04-09 17:30:04 +0200

    make managers singleton

--- Case3/Case3/UnicontaManager.cs
+++ Case3/Case3/UnicontaManager.cs
@@ -11,22 +11,30 @@ using Uniconta.DataModel;
 
 namespace Case3
 {
-    public static class UnicontaManager
+    public class UnicontaManager
     {
-        public static UnicontaConnection Connection { get; private set; }
-        public static Session Session { get; private set; }
+        public UnicontaConnection Connection { get; private set; }
+        public Session Session { get; private set; }
         // TODO: Insert API Key
-        private static Guid APIKey { get; set; } = new Guid("00000000-0000-0000-0000-000000000000");
-        public static CrudAPI CrudAPI { get; private set; }
-        public static Company CurrentCompany { get; private set; }
+        private Guid APIKey { get; set; } = new Guid("00000000-0000-0000-0000-000000000000");
+        public CrudAPI CrudAPI { get; private set; }
+        public Company CurrentCompany { get; private set; }
 
-        public static void Initialize()
+        private static UnicontaManager _instance;
+
+        public static UnicontaManager GetInstance()
+        {
+            if (_instance == null) _instance = new UnicontaManager();
+            return _instance;
+        }
+
+        public void Initialize()
         {
             Connection = new UnicontaConnection(APITarget.Live);
             Session = new Session(Connection);
         }
 
-        public static async Task<bool> Login(string username, string password)
+        public async Task<bool> Login(string username, string password)
         {
             // Attempt to login
             var result = await Session.LoginAsync(username, password, LoginType.API, APIKey);
@@ -41,12 +49,12 @@ namespace Case3
             return true;
         }
 
-        public static async Task Logout()
+        public async Task Logout()
         {
             await Session.LogOut();
         }
 
-        private static async Task InitializeCompany()
+        private async Task InitializeCompany()
         {
             // If Session has a default company, use DefaultComapny as CurrentCompany
             if (Session.DefaultCompany != null)
--- Case4.Unsolved/Case4.Unsolved/UnicontaManager.cs
+++ Case4.Unsolved/Case4.Unsolved/UnicontaManager.cs
@@ -11,52 +11,46 @@ using Uniconta.DataModel;
 
 namespace Case4.Unsolved
 {
-    public static class UnicontaManager
+    public class UnicontaManager
     {
-        public static UnicontaConnection Connection { get; private set; }
-        public static Session Session { get; private set; }
+        private static UnicontaManager _instance;
+
+        public UnicontaConnection Connection { get; private set; }
+        public Session Session { get; private set; }
+        public CrudAPI CrudAPI { get; private set; }
+        public Company CurrentCompany { get; private set; }
         // TODO: Insert API Key
         private static Guid APIKey { get; set; } = new Guid("00000000-0000-0000-0000-000000000000");
-        public static CrudAPI CrudAPI { get; private set; }
-        public static Company CurrentCompany { get; private set; }
 
-        public static void Initialize()
+        private UnicontaManager() { }
+
+        public static UnicontaManager GetInstance()
         {
-            Connection = new UnicontaConnection(APITarget.Live);
-            Session = new Session(Connection);
+            if (_instance == null) _instance = new UnicontaManager();
+            return _instance;
         }
 
-        public static async Task<bool> Login(string username, string password)
+        public void Initialize()
         {
-            // Attempt to login
-            var result = await Session.LoginAsync(username, password, LoginType.API, APIKey);
-
-            // If Login was not succesful, return false
-            if (result != ErrorCodes.Succes) return false;
-
-            // Initialize Company, and new-up CrudAPI
-            await InitializeCompany();
-            CrudAPI = new CrudAPI(Session, CurrentCompany);
-
-            return true;
+            // TODO: Initialize UnicontaManager
         }
 
-        public static async Task Logout()
+        public async Task<bool> Login(string username, string password)
         {
-            await Session.LogOut();
+            // TODO: Program login logic (You can change the return type if you'd like)
+            return false;
         }
 
-        private static async Task InitializeCompany()
+        public async Task Logout()
         {
-            // If Session has a default company, use DefaultComapny as CurrentCompany
-            if (Session.DefaultCompany != null)
-            {
-                CurrentCompany = Session.DefaultCompany;
-                return;
-            }
-
-            // TODO: Change Company ID 0 to your company's ID
-            CurrentCompany = await Session.OpenCompany(-1, false);
+            // TODO: Program logout logic
         }
+
+        private async Task InitializeCompany()
+        {
+            // TODO: Program logic for intializing current company
+            // Hint: Session has a property called DefaultCompany, which is initialized on login if the user has set a default company.
+        }
+
     }
 }
